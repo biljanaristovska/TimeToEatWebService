5d9dda89b6c888b49481ad99c8e4d6e059bb7618
biljanaristovska <biljana_ristovska91@yahoo.com>
2014-08-10 15:45:36 +0200

    Test

--- Service1.svc.cs
+++ Service1.svc.cs
@@ -14,7 +14,7 @@ namespace TimeToEatWebService
     {
         public string GetData(int value)
         {
-            return string.Format("You entered: {0}", value);
+            return string.Format("You entered: {0}", value == 69 ? "Sex" : value.ToString());
         }
     }
 }
